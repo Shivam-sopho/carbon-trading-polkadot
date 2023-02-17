926c18c7eecba9a62034aa976ecaea01515de7a2
Shubham Prasad <prasadshubham821@gmail.com>
2023-02-17 20:00:52 +0530

    Add pages

---
+++ packages/frontend/src/pages/generate-carbon-token.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next'
+
+const GenerateCarbonTokenPage: NextPage = () => {
+  return <></>
+}
---
+++ packages/frontend/src/pages/repay-loan.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next'
+
+const RepayLoanPage: NextPage = () => {
+  return <></>
+}
